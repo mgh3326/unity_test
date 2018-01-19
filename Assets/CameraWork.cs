dd9af96546812c272e6ffef09a6621a98e014c9d
mgh3326 <mgh3326@naver.com>
2018-01-20 01:11:13 +0900

    update

--- Assets/CameraWork.cs
+++ Assets/CameraWork.cs
@@ -9,12 +9,21 @@ public class CameraWork : MonoBehaviour
     void Start()
     {
         ball = GameObject.Find("Ball");
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+
+        //Debug.Log(coins[0].name);
+        //Debug.Log(coins[5].name);
+        for (int i = 0; i < coins.Length; i++)
+        {
+            Debug.Log(coins[i].name);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log("I am Camera. And ball is at " + ball.transform.position.z);
-        transform.position = new Vector3(0, ball.transform.position.y+3, ball.transform.position.z - 14);
+        transform.position = new Vector3(0, ball.transform.position.y + 3, ball.transform.position.z - 14);
     }
 }
---
+++ Assets/RedCoin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCoin : MonoBehaviour {
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Ball")
+        {
+            DestroyObstacles();
+            Destroy(gameObject);
+        }
+
+    }
+
+    void DestroyObstacles()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            Destroy(obstacles[i]);
+
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
