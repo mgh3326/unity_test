2581fd7b519534e63690a1d4a8393ca571fe3321
mgh3326 <mgh3326@naver.com>
2018-01-20 00:44:25 +0900

    update

---
+++ Assets/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Ball")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/FailZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailZone : MonoBehaviour {
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.name=="Ball")
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel(name: "Game");
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
